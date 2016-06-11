bc919f2c5b00ffb2da3078dae7447da6112ebacf
kol@llol <kol@llol>
2016-06-11 17:08:26 +0200

    WIECEJ NIE UMIEM

--- src/com/company/ZajezdniaAutobusowa.java
+++ src/com/company/ZajezdniaAutobusowa.java
@@ -5,16 +5,23 @@ package com.company;
  */
 public class ZajezdniaAutobusowa extends Pojazdy {
     private String jakaZajezdnia;
+    private int ilePaliwa;
 
-    public ZajezdniaAutobusowa(int numerPojazdu, int predkoscMax, String zajezdnia, String jakaZajezdnia) {
+    public ZajezdniaAutobusowa(int numerPojazdu, int predkoscMax, String zajezdnia, String jakaZajezdnia, int ilePaliwa) {
         super(numerPojazdu, predkoscMax, zajezdnia);
         this.jakaZajezdnia = jakaZajezdnia;
+        this.ilePaliwa = ilePaliwa;
+    }
+
+    public int SumaSpalania() {
+        return ilePaliwa;
     }
 
     @Override
     public String toString() {
         return "ZajezdniaAutobusowa{" +
                 "jakaZajezdnia='" + jakaZajezdnia + '\'' +
+                ", ilePaliwa=" + ilePaliwa +
                 '}';
     }
 }
--- src/com/company/ZajezdniaTramwajowa.java
+++ src/com/company/ZajezdniaTramwajowa.java
@@ -5,16 +5,23 @@ package com.company;
  */
 public class ZajezdniaTramwajowa extends Pojazdy {
     private String jakaZajezdnia;
+    private int iloscWagonow;
 
-    public ZajezdniaTramwajowa(int numerPojazdu, int predkoscMax, String zajezdnia, String jakaZajezdnia) {
+    public ZajezdniaTramwajowa(int numerPojazdu, int predkoscMax, String zajezdnia, String jakaZajezdnia, int iloscWagonow) {
         super(numerPojazdu, predkoscMax, zajezdnia);
         this.jakaZajezdnia = jakaZajezdnia;
+        this.iloscWagonow = iloscWagonow;
+    }
+
+    public int SumaWagonow() {
+        return iloscWagonow;
     }
 
     @Override
     public String toString() {
         return "ZajezdniaTramwajowa{" +
                 "jakaZajezdnia='" + jakaZajezdnia + '\'' +
+                ", iloscWagonow=" + iloscWagonow +
                 '}';
     }
 }
