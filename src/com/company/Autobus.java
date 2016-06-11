b19bce2ce8fe54a13ddb5c7fa565019c37671889
kol@llol <kol@llol>
2016-06-11 16:43:47 +0200

    pojazdy gotowe autobusy i tramwaje tez

--- src/com/company/Autobus.java
+++ src/com/company/Autobus.java
@@ -6,7 +6,19 @@ package com.company;
 public class Autobus extends Pojazdy{
     private int ilePaliwaZuzyl;
 
-    public Autobus(final String numerPojazdu, final String PredkoscMax, final String Zajezdnia, final int ilePaliwaZuzyl){
+    public Autobus(final String numerPojazdu, final String predkoscMax, final String zajezdnia, final int ilePaliwaZuzyl){
+        super(numerPojazdu, predkoscMax, zajezdnia);
+        this.ilePaliwaZuzyl = ilePaliwaZuzyl;
+    }
 
+    public int getIlePaliwaZuzyl() {
+        return ilePaliwaZuzyl;
+    }
+
+    @Override
+    public String toString() {
+        return "Autobus{" +
+                "ilePaliwaZuzyl=" + ilePaliwaZuzyl +
+                '}';
     }
 }
--- src/com/company/Tramwaj.java
+++ src/com/company/Tramwaj.java
@@ -3,8 +3,24 @@ package com.company;
 /**
  * Created by ced on 2016-06-11.
  */
-public class Tramwaj extends Pojazdy{
+public class Tramwaj extends Pojazdy {
     private int ileWagonow;
+
+    public Tramwaj(final String numerPojazdu, final String predkoscMax, final String zajezdnia, final int ileWagonow){
+        super(numerPojazdu, predkoscMax, zajezdnia);
+        this.ileWagonow = ileWagonow;
+    }
+
+    public int getIleWagonow() {
+        return ileWagonow;
+    }
+
+    @Override
+    public String toString() {
+        return "Tramwaj{" +
+                "ileWagonow=" + ileWagonow +
+                '}';
+    }
 }
 
 
