c12b9a4e65ecc4b595a0489adb3566d2d9847441
kol@llol <kol@llol>
2016-06-11 17:11:10 +0200

    WIECEJ NIE UMIEM

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Pojazdy p1 = new Pojazdy(10, 200, "tramwajowa");
         System.out.println(p1);
-        Autobus a1 = new Autobus(2, 43, "tramwajowa", 100);
-        System.out.println(a1);
+
     }
 }
