5a8e775508c91b571c6c8aaa825c9697f0b9f34c
kol@llol <kol@llol>
2016-06-11 16:46:07 +0200

    pojazdy gotowe autobusy i tramwaje tez

--- src/com/company/ZajezdniaAutobusowa.java
+++ src/com/company/ZajezdniaAutobusowa.java
@@ -3,5 +3,8 @@ package com.company;
 /**
  * Created by ced on 2016-06-11.
  */
-public class ZajezdniaAutobusowa {
+public class ZajezdniaAutobusowa extends Pojazdy {
+    public ZajezdniaAutobusowa(String numerPojazdu, String predkoscMax, String zajezdnia) {
+        super(numerPojazdu, predkoscMax, zajezdnia);
+    }
 }
---
+++ src/com/company/ZajezdniaTramwajowa.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by ced on 2016-06-11.
+ */
+public class ZajezdniaTramwajowa extends Pojazdy {
+    public ZajezdniaTramwajowa(String numerPojazdu, String predkoscMax, String zajezdnia) {
+        super(numerPojazdu, predkoscMax, zajezdnia);
+    }
+}
